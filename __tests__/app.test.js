a17aa5e255b4c04a494b35412eac23831e48c8de
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-14 18:13:49 -0700

    first GET and POST pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,19 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  it('GET hi, plain text from /', async() => {
+    const res = await request(app).get('/');
 
+    expect(res.text).toEqual('hi');
+  });
+
+  it('POST to /echo', async() => {
+    const res = await request(app)
+      .post('/echo')
+      .send({ body: 'hi', status: '200', contentType: 'plain/text' });
+    
+    const response = await request(app)
+      .get('/echo');
+    expect(response.text).toEqual('hi');
+  });
 });
