7d2ca06dec386fb93b8e93e5045d2a782b67663a
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-15 09:37:07 -0700

    refactored parseRequest, color routes passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const parseRequest = require('../lib/utils/parseRequest');
 
 describe('app routes', () => {
   it('GET hi, plain text from /', async() => {
@@ -19,9 +20,71 @@ describe('app routes', () => {
   });
 
   it('GET red', async() => {
+    // const red = await request(app)
+    //   .post('/red')
+    //   .send({ body: , status: '200', contentType: 'plain/text' });
+
     const res = await request(app)
       .get('/red');
     
-    expect(res.text).toEqual('<h1>red</h1>');
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> red </h1>
+</body>
+</html>`);
+    
+  });
+
+  it('GET green', async() => {
+    // const red = await request(app)
+    //   .post('/red')
+    //   .send({ body: , status: '200', contentType: 'plain/text' });
+
+    const res = await request(app)
+      .get('/green');
+    
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> green </h1>
+</body>
+</html>`);
+    
+  });
+
+  it('GET blue', async() => {
+    // const red = await request(app)
+    //   .post('/red')
+    //   .send({ body: , status: '200', contentType: 'plain/text' });
+
+    const res = await request(app)
+      .get('/blue');
+    
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1> blue </h1>
+</body>
+</html>`);
+    
   });
 });
