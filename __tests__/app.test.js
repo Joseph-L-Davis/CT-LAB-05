9326725ffd06ef9cf145d1ef90fd7339caa68e13
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-16 16:28:12 -0700

    test created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -86,4 +86,21 @@ describe('app routes', () => {
 </html>`);
     
   });
+
+  it('GET html  /index.html', async() => {
+    const res = await request(app).get('/index.html');
+
+    expect(res.text).toEqual(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>AM I DOING THIS RIGHT???</p>
+</body>
+</html>`);
+  });  
 });
