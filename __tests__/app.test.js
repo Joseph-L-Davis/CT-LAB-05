87b534693eb4dc55fd3e8476541762a55c93e06d
Joseph Davis <joethebartenderpdx@gmail.com>
2021-06-16 17:19:59 -0700

    had to create a test without the function first. time to refactor

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,12 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('app routes', () => {
-  it('GET html  /index.html', async() => {
-    const res = await request(app).get('/index.html');
+describe('app routes', () => { 
+  it('GET text from /index.html', async() => {
+    const res = await request(app)
+      .get('/index.html');
 
-    expect(res.text).toEqual(`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>AM I DOING THIS RIGHT???</p>
-</body>
-</html>`);
+    console.log(res.text);
+    expect(res.text).toEqual('AM I DOING THIS RIGHT?');
   });  
 });
